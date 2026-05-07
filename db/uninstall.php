074f623882383ba8368cb15f7dbbd5d1d5a6575f
Ahmad Nawid <ahmadnawid.mz@gmail.com>
2026-05-07 12:22:59 +0200

    Remove unnecessary code from uninstall.php

--- db/uninstall.php
+++ db/uninstall.php
@@ -21,9 +21,6 @@
  * @copyright  2026 Ahmad Nawid Mustafazada <ahmadnawid.mz@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Custom uninstallation procedure.
  *
